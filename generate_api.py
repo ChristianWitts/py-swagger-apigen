a3df5eee405579860b24a52fe98093b9a6f9f966
Christian Witts <cwitts@gmail.com>
2016-05-17 10:57:21 +0200

    Added globals to stop passing through so many params.

--- generate_api.py
+++ generate_api.py
@@ -7,6 +7,9 @@ import json
 import click
 from jinja2 import Template
 
+PROJECT = None
+TARGET = None
+
 
 def load_spec(filename):
     """Load the Swagger specification and return the config
@@ -23,14 +26,16 @@ def load_spec(filename):
             return yaml.load(f)
 
 
-def create_base(destination):
-    if not os.path.exists(destination):
-        os.mkdir(destination)
+def create_base():
+    if not os.path.exists(PROJECT):
+        os.mkdir(PROJECT)
 
     # Copy requirements
-    shutil.copy('templates/falcon/requirements.tpl', '{}/requirements.txt'.format(destination))
+    shutil.copy('templates/{}/requirements.tpl'.format(TARGET),
+                '{}/requirements.txt'.format(PROJECT))
     # Copy middleware
-    shutil.copy('templates/falcon/middleware.tpl', '{}/middleware.py'.format(destination))
+    shutil.copy('templates/{}/middleware.tpl'.format(TARGET),
+                '{}/middleware.py'.format(PROJECT))
 
 
 def get_resource_name(path_spec):
@@ -39,33 +44,33 @@ def get_resource_name(path_spec):
                                        if part and part[0] != '{'))
 
 
-def generate_validation_schemas(destination, definitions):
+def generate_validation_schemas(definitions):
     schemas = ((schema_name, json.dumps(schema_spec, indent=4))
                 for schema_name, schema_spec in definitions.iteritems())
 
-    with open('templates/falcon/schemas.tpl', 'r') as s:
+    with open('templates/{}/schemas.tpl'.format(TARGET), 'r') as s:
         template = Template(s.read())
 
-    with open('{}/schemas.py'.format(destination), 'w') as s:
+    with open('{}/schemas.py'.format(PROJECT), 'w') as s:
         s.write(template.render(schemas=schemas))
 
 
-def generate_entrypoint(destination, routing_table):
+def generate_entrypoint(routing_table):
     middlewares = ('RequireJSON', 'JSONTranslator')
-    with open('templates/falcon/main.tpl', 'r') as t:
+    with open('templates/{}/main.tpl'.format(TARGET), 'r') as t:
         template = Template(t.read())
 
-    with open('{}/main.py'.format(destination), 'w') as f:
+    with open('{}/main.py'.format(PROJECT), 'w') as f:
         f.write(template.render(routing_table=routing_table, middlewares=middlewares))
 
 
-def generate_api(destination, config):
+def generate_api(config):
     routing_table = []
     for path_spec, endpoints in config['paths'].iteritems():
         resource_name = get_resource_name(path_spec)
         routing_table.append((path_spec, resource_name))
 
-    generate_entrypoint(destination, routing_table)
+    generate_entrypoint(routing_table)
 
 
 def copy_ui(destination):
@@ -79,8 +84,10 @@ def copy_ui(destination):
 @click.command()
 @click.argument("destination", required=True)
 @click.option("-s", "--swagger", required=True, help="Swagger spec")
+@click.option("-t", "--target", required=True, help="Target framework",
+              type=click.Choice(["falcon", "flask"]))
 @click.option("--ui", default=False, is_flag=False, help="Generate swagger UI.")
-def generate(destination, swagger, ui=False):
+def generate(destination, swagger, target, ui=False):
     """Main entry point to generate your API based off of the swagger documentation.
 
     :param str destination: Your project name.
@@ -88,12 +95,18 @@ def generate(destination, swagger, ui=False):
     :param bool ui: If you want to have a swagger UI generated.
     :return: None
     """
+    # Set some globals for convenience
+    global PROJECT
+    PROJECT = destination
+    global TARGET
+    TARGET = target
+
     spec = load_spec(swagger)
-    create_base(destination)
+    create_base()
     if ui:
-        copy_ui(destination)
-    generate_validation_schemas(destination, spec['definitions'])
-    generate_api(destination, spec)
+        copy_ui()
+    generate_validation_schemas(spec['definitions'])
+    generate_api(spec)
 
 
 if __name__ == '__main__':
